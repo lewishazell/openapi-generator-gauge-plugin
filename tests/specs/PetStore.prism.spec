126a72bca792fd0bc8e9692cc3a5bf47d33cb2e2
Lewis Hazell <git@hazell.dev>
2025-08-31 16:27:43 +0100

    test: added tests for failed assertions with snapshots to verify output

--- tests/specs/PetStore.prism.spec
+++ tests/specs/PetStore.prism.spec
@@ -10,6 +10,24 @@ This scenario ensures that the user can successfully retrieve a pet from the sto
 * The response status should be "200"
 * The response content should be "{\"name\":\"string\",\"tag\":\"string\",\"id\":-9007199254740991}"
 
+## Successfully find a pet at the pet store with an incorrect response status
+This scenario ensures that the user can successfully retrieve a pet from the store by its ID, but asserts an incorrect response status.
+
+* Create a "findPetById" request for the "DefaultApi"
+    * Give the "id" parameter a value of "1"
+* Send the request
+* The response status should be "201"
+* The response content should be "{\"name\":\"string\",\"tag\":\"string\",\"id\":-9007199254740991}"
+
+## Successfully find a pet at the pet store with an incorrect response content
+This scenario ensures that the user can successfully retrieve a pet from the store by its ID, but asserts an incorrect response content.
+
+* Create a "findPetById" request for the "DefaultApi"
+    * Give the "id" parameter a value of "1"
+* Send the request
+* The response status should be "200"
+* The response content should be "{\"name\":\"Pickle\",\"tag\":\"cat\",\"id\":1}"
+
 ## Successfully delete a pet from the pet store
 This scenario verifies that a pet can be deleted successfully from the pet store using its ID.
 
--- tests/test_codegen.sh
+++ tests/test_codegen.sh
@@ -14,44 +14,51 @@ setUp() {
 testCSharpCodegen() {
     filter "$FUNCNAME" || return 0
 
+    local testdir="out/src/PetStore.Test"
     openapi-generator-cli --custom-generator ../target/openapi-generator-gauge-plugin-1.0.0.jar generate -g csharp-gauge --package-name PetStore -i petstore-extended.yaml -o out -p gaugeTargetHost=http://localhost:4010
-    mv out/specs out/src/PetStore.Test
-    (cd out/src/PetStore.Test && gauge run specs) || fail "Gauge run failed"
+    mv "out/specs" "$testdir"
+    (cd "$testdir" && gauge run specs)
+    (cat "$testdir/reports/json-report/result.json" | scrubGaugeReport | verifyJson) || fail "Received report differed from verified snapshot"
 }
 
 testGoCodegen() {
     filter "$FUNCNAME" || return 0
 
     openapi-generator-cli --custom-generator ../target/openapi-generator-gauge-plugin-1.0.0.jar generate -g go-gauge --package-name petstore -i petstore-extended.yaml -o out -p gaugeTargetHost=http://localhost:4010
-    (cd out && go get -u -v all && gauge run specs) || fail "Gauge run failed"
+    (cd out && go get -u -v all && gauge run specs)
+    (cat "out/reports/json-report/result.json" | scrubGaugeReport | verifyJson) || fail "Received report differed from verified snapshot"
 }
 
 testJavaCodegenWithGradle() {
     filter "$FUNCNAME" || return 0
 
     openapi-generator-cli --custom-generator ../target/openapi-generator-gauge-plugin-1.0.0.jar generate -g java-gauge --package-name PetStore -i petstore-extended.yaml -o out -p gaugeTargetHost=http://localhost:4010
-    (cd out && gradle gauge) || fail "Gauge run failed"
+    (cd out && gradle gauge)
+    (cat "out/reports/json-report/result.json" | scrubGaugeReport | verifyJson) || fail "Received report differed from verified snapshot"
 }
 
 testJavaCodegenWithMaven() {
     filter "$FUNCNAME" || return 0
 
     openapi-generator-cli --custom-generator ../target/openapi-generator-gauge-plugin-1.0.0.jar generate -g java-gauge --package-name PetStore -i petstore-extended.yaml -o out -p gaugeTargetHost=http://localhost:4010
-    (cd out && mvn test-compile gauge:execute) || fail "Gauge run failed"
+    (cd out && mvn test-compile gauge:execute)
+    (cat "out/reports/json-report/result.json" | scrubGaugeReport | verifyJson) || fail "Received report differed from verified snapshot"
 }
 
 testPythonCodegen() {
     filter "$FUNCNAME" || return 0
 
     openapi-generator-cli --custom-generator ../target/openapi-generator-gauge-plugin-1.0.0.jar generate -g python-gauge --package-name petstore -i petstore-extended.yaml -o out -p gaugeTargetHost=http://localhost:4010
-    (cd out && pip install -r requirements.txt -r test-requirements.txt && gauge run specs) || fail "Gauge run failed"
+    (cd out && pip install -r requirements.txt -r test-requirements.txt && gauge run specs)
+    (cat "out/reports/json-report/result.json" | scrubGaugeReport | verifyJson) || fail "Received report differed from verified snapshot"
 }
 
 testTypeScriptNodeCodegen() {
     filter "$FUNCNAME" || return 0
 
     openapi-generator-cli --custom-generator ../target/openapi-generator-gauge-plugin-1.0.0.jar generate -g typescript-node-gauge -i petstore-extended.yaml -o out -p gaugeTargetHost=http://localhost:4010 -p npmName=petstore
-    (cd out && npm install && gauge run specs) || fail "Gauge run failed"
+    (cd out && npm install && gauge run specs)
+    (cat "out/reports/json-report/result.json" | scrubGaugeReport | verifyJson) || fail "Received report differed from verified snapshot"
 }
 
 tearDown() {
@@ -71,6 +78,28 @@ filter() {
   fi
 
   return 0
+}
+
+scrubGaugeReport() {
+    cat | jq '
+        del(.timestamp, .timestampISO, .executionTime) |
+        .specResults |= map(
+            del(.timestampISO, .executionTime, .fileName) |
+            .scenarios |= map(
+                del(.executionTime) |
+                .items |= map(
+                    .result |= del(.stackTrace, .executionTime, .screenshot, .ScreenshotFile, .errorMessage, .messages, .skippedReason)
+                )
+            )
+        )
+    '
+}
+
+verifyJson() {
+    local snapshotpath="snapshots/${FUNCNAME[1]}.verified.json"
+    local receivedpath="${snapshotpath/.verified.json/.received.json}"
+
+    cat | tee "$receivedpath" | jd "$snapshotpath"
 }
 
 . /usr/share/shunit2/shunit2
